adbe60f695cd1a1949b693283ba69e5a56bbfd30
savent <savent_gate@outlook.com>
2018-08-13 22:21:10 +0800

    添加 HSV 用于 RGB的色相转换
     - 实现 RGB<->HSV 的类型转换

--- Inc/color.hpp
+++ Inc/color.hpp
@@ -139,3 +139,109 @@ class RGB
         }
     }
 };
+
+typedef struct HSV
+{
+    float h;
+    float s;
+    float v;
+    HSV(uint8_t _h, uint8_t _s, uint8_t _v)
+    {
+        h = _h;
+        s = _s;
+        v = _v;
+    }
+    HSV(RGB& rgb)
+    {
+        *this = rgb;
+    }
+
+    RGB convert2RGB()
+    {
+        // In-case h is negative
+        while (h < 0)
+            h += 360.0f;
+        float C = v * s;
+        float P = fmodf(h / 60.0f, 6.0f);
+        float X = C * (1 - fabs(fmodf(P, 2) - 1));
+        float m = v - C;
+        C += m;
+        X += m;
+
+        if (h < 60) {
+            return RGB(C * 255, X * 255, m * 255);
+        } else if (h < 120) {
+            return RGB(X * 255, C * 255, m * 255);
+        } else if (h < 180) {
+            return RGB(m * 255, C * 255, X * 255);
+        } else if (h < 240) {
+            return RGB(m * 255, X * 255, C * 255);
+        } else if (h < 300) {
+            return RGB(X * 255, m * 255, C * 255);
+        } else if (h < 360) {
+            return RGB(C * 255, m * 255, X * 255);
+        } else {
+            return RGB(0, 0, 0);
+        }
+    }
+
+    HSV& operator=(const HSV& other)
+    {
+        if (&other == this)
+        {
+            return *this;
+        }
+        h = other.h;
+        s = other.s;
+        v = other.v;
+    }
+
+    HSV& operator =(const RGB& rgb)
+    {
+        float r = rgb.R / 255.0f;
+        float g = rgb.G / 255.0f;
+        float b = rgb.B / 255.0f;
+
+        int pmax = 2;
+        float max = b, min = b;
+        if (max < r) {
+            pmax = 0;
+            max = r;
+        }
+        if (min > r) {
+            min = r;
+        }
+        if (max < g) {
+            max = g;
+            pmax = 1;
+        }
+        if (min > g) {
+            min = g;
+        }
+        float delta = max - min;
+
+        if (delta <= float(1e-4) && delta >= float(-1e-4)) {
+            h = 0;
+        } else if (pmax == 0) {
+            h = (int((g - b) / delta) % 6) * 60.0f;
+        } else if (pmax == 1) {
+            h = (((b - r) / delta) + 2) * 60.0f;
+        } else if (pmax == 2) {
+            h = (((r - g) / delta) + 4) * 60.0f;
+        }
+
+        if (max <= float(1e-4) && max >= float(-1e-4)) {
+            s = 0;
+        } else {
+            s = delta / max;
+        }
+
+        v = max;
+        return *this;
+    }
+
+    operator class RGB()
+    {
+        return this->convert2RGB();
+    }
+} HSV;
