2156c6213ca41caefaa217156203a8f19946c45a
savent <savent_gate@outlook.com>
2019-01-28 16:10:47 +0800

    add iEvent's test suit

---
+++ test/iEvent_test.cpp
@@ -0,0 +1,59 @@
+#include "iEvent.hpp"
+
+#include <queue>
+
+static std::queue<uint32_t> message_quque;
+
+void defaultEventSender(uint32_t message)
+{
+    message_quque.push(message);
+    return true;
+}
+
+bool defaultEventReciver(uint32_t&message, uint32_t timeout = 100)
+{
+    if (message_quque.empty())
+        return false;
+    message = message_quque.front();
+    message_quque.pop();
+    return true;
+}
+
+TEST(iEventTest, checkMash)
+{
+    iEvent event(0);
+    ASSERT_EQ(event.getEventMask(), 0xFFFF);
+    for (int i = 0; i < 16; i++) {
+        uint16_t mask = 1 << i;
+        event.setEventMask(mask);
+
+        ASSERT_EQ(event.getEventMask(), mask);
+    }
+}
+
+TEST(iEventTest, checkModuleID)
+{
+    for (int i = 0; i < 16; i++)
+    {
+        uint16_t moduleID = 1 << i;
+        iEvent event(moduleID);
+        ASSERT_EQ(event.getModuleID(), moduleID);
+    }
+}
+
+TEST(iEventTest, checkSignals)
+{
+    iEvent event_key(EVENT_MODULE_ID_KEY);
+    event_key
+    for (int i = 0; i < 16; i++) {
+        uint16_t event = (1 << i);
+
+        event_key.sendEvent(event);
+    }
+    for (int i = 0; i < 16; i++) {
+        uint16_t except_event = 1 << i;
+        uint32_t message; 
+        ASSERT_EQ(true, defaultEventReciver(message));
+        ASSERT_EQ(except_event, iEvent::getEvent(message));
+    }
+}
